c00822228ad37868e494cc8321713c8a6780c5f5
Egerzvon <49019763+Egerzvon@users.noreply.github.com>
2019-05-23 22:46:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 const std::string SPECS[] = {"year","color","engine capacity","akpp//mkpp","engine power","fuel tank","fuel consumption","dimensions","heated steering wheel","abs","eds","navigation system","board computer","heated seats","roof rack","number of airbags"};
+
+
+
+class automobile
+{
+public:
+    std::vector<std::string> params;
+    automobile(std::ifstream& file){this->read(file);}
+    void read(std::ifstream& file)
+    {
+        std::string buf;
+        for(auto&& specification : SPECS){
+            std::getline(file,buf);
+            params.push_back(buf);
+        }
+    }
+    void print(){
+        for(int i=0; i<params.size(); i++)
+            std::cout << SPECS[i] << " : " << params[i] << std::endl;   
+    }
+    
+};
 
 class handler
 {
 public:
-    handler* next;
-    std::string filename;   
-    handler(std::string filename):filename(filename),next(nullptr){};
-    void find_automobile(std::string model)
+    std::string filename;  
+
+    handler(std::string filename):filename(filename){};
+    bool find_automobile(std::string model)
     {
         std::ifstream file(this->filename);
         bool found=false;
@@ -21,36 +44,41 @@ public:
             {
                 found=true;
                 std::cout << this->filename << ": found!" << std::endl;
-                for(auto&& specification : SPECS)
-                {
-                    std::getline(file,buf);
-                    std::cout << specification << " : " << buf << std::endl;
-                }
+                automobile founded(file);
+                founded.print();
+                return true;
             }
         }
-        if(!found)
-        {
-            if(next!=nullptr){
-                std::cout << this->filename << ": tryind to find deeper" << std::endl;
-                next->find_automobile(model);
-            }
-            else
-                std::cout << this->filename << ": not found :С" << std::endl;
-        }
-    }
-    void set_next(handler* next_handler){this->next=next_handler;};
+        if(!found) return false;
 
+    }     
+
+};
+
+class reciver
+{
+    private:
+    std::vector<handler> v;
+    public:
+    reciver(handler begin){this->set_next(begin);}
+    void set_next(handler next){v.push_back(next);}
+    void find(std::string model)
+    {
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            if(it->find_automobile(model)) return;
+            else std::cout<<it->filename<<" not found"<<std::endl;
+        }
+        std::cout << "Model does not exist" << std::endl;
+        return;
+    }
 };
 
 
 int main(int argc, char const *argv[])
 {
-    handler* salon = new handler("salon");
-    salon->set_next(new handler("dealer"));
-    salon->next->set_next(new handler("manufacture"));
-    std::string model;
-    std::cout << "Enter model to find:";
-    std::cin >> model;
-    salon->find_automobile(model);
+    reciver autosalon(handler("salon"));
+    autosalon.set_next(handler("dealer"));
+    autosalon.set_next(handler("manufacture"));
+    autosalon.find("creta");
     return 0;
 }
